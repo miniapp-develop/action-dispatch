6c53d94afb167bde26dfc041e3ee594cc2bb633d
xesam <xesamguo@gmail.com>
2022-05-25 10:52:29 +0800

    web-view /fix

--- pages/web/index.js
+++ pages/web/index.js
@@ -5,5 +5,8 @@ Page({
     onLoad(query) {
         console.log('内置webview', query);
         console.log('内置webview,decode url=', decodeURIComponent(query._url));
+        this.setData({
+            src: decodeURIComponent(query._url)
+        });
     }
 });
